86e6a1dd979382f20d9afd83b32b12929da90abd
cs-ddjor001 <ddjor001@odu.edu>
2024-03-18 18:39:23 -0400

    Final Changes and Tests for now

--- Duals.cpp
+++ Duals.cpp
@@ -8,6 +8,48 @@ inline T SimpleFunction(const T& x)
     return x * x * (T(3.0) - T(2.0) * x);
 }
 
+void TestMath(float input)
+{
+    // create a dual number as the value of x
+    Duals<1, float> x(input, 1);
+
+    // sqrt
+    {
+        // calculate value and derivative using dual numbers
+        Duals<1, float> y = Duals<1, float>(3.0f) / sqrt(x);
+
+        // calculate numeric derivative using central differences
+        float derivNumeric = ((3.0f / sqrt(input + EPSILON)) - (3.0f / sqrt(input - EPSILON))) / (2.0f * EPSILON);
+
+        // calculate actual derivative
+        float derivActual = -3.0f / (2.0f * pow(input, 3.0f / 2.0f));
+
+        // show value and derivatives
+        cout << "3/sqrt(" << input << ") = " << y.getValue() << "\n";
+        cout << "  dual# dy/dx = " << y.getDerivative() << "\n";
+        cout << "  actual dy/dx = " << derivActual << "\n";
+        cout << "  numeric dy/dx = " << derivNumeric << "\n";
+    }
+
+    // pow
+    {
+        // calculate value and derivative using dual numbers
+        Duals<1, float> y = pow(x + Duals<1, float>(1.0f), 1.337f);
+
+        // calculate numeric derivative using central differences
+        float derivNumeric = ((pow(input + 1.0f + EPSILON, 1.337f)) - (pow(input + 1.0f - EPSILON, 1.337f))) / (2.0f * EPSILON);
+
+        // calculate actual derivative
+        float derivActual = 1.337f * pow(input + 1.0f, 0.337f);
+
+        // show value and derivatives
+        cout << "(" << input << "+1)^1.337 = " << y.getValue() << "\n";
+        cout << "  dual# dy/dx = " << y.getDerivative() << "\n";
+        cout << "  actual dy/dx = " << derivActual << "\n";
+        cout << "  numeric dy/dx = " << derivNumeric << "\n";
+    }
+}
+
 template <typename T>
 Duals<2, T> TwoD(const Duals<2, T>& x, const Duals<2, T>& y)
 {
@@ -24,7 +66,7 @@ Duals<2, T> TwoD(const Duals<2, T>& x, const Duals<2, T>& y)
 }
 
 template <size_t VARIABLES, typename T>
-void TestSmoothStep(T input) 
+void TestSimpleFunction(T input) 
 {
     // Create a dual number as the value of x
     Duals<VARIABLES, T> x(input, 1);
@@ -58,8 +100,8 @@ void TestTwoD(T x_val, T y_val)
     // Show value and derivatives
     cout << "(2D) z = x^2 * y + 2x  (x = " << x_val << ", y = " << y_val << ")\n";
     cout << "  z = " << z.getValue() << '\n';
-    cout << "  dz/dx = " << z.getDerivative(0) << "  (expected: 14)\n";
-    cout << "  dz/dy = " << z.getDerivative(1) << "  (expected: 4)\n";
+    cout << "  dz/dx = " << z.getDerivative(0) << "\n";
+    cout << "  dz/dy = " << z.getDerivative(1) << "\n";
 }
 
 template <typename T>
@@ -75,7 +117,8 @@ void Test2D(T inputx, T inputy)
         Duals<2, T> z = Duals<2, T>(3.0) * x * x - Duals<2, T>(2.0) * y * y * y;
 
         // Calculate numeric partial derivatives using central differences
-        auto f = [](T x, T y) {
+        auto f = [](T x, T y) 
+        {
             return 3.0 * x * x - 2.0 * y * y * y;
         };
         T derivNumericX = (f(inputx + EPSILON, inputy) - f(inputx - EPSILON, inputy)) / (2.0 * EPSILON);
@@ -97,16 +140,140 @@ void Test2D(T inputx, T inputy)
     }
 }
 
+void Test3D(float inputx, float inputy, float inputz)
+{
+    // Create dual numbers as the value of x, y, and z
+    Duals<3, float> x(inputx, {1, 0, 0}); // Setting derivative of x to 1
+    Duals<3, float> y(inputy, {0, 1, 0}); // Setting derivative of y to 1
+    Duals<3, float> z(inputz, {0, 0, 1}); // Setting derivative of z to 1
+    Duals<3, float> constant2(2.0f, {0, 0, 0});
+
+    // w = sin(x*cos(2*y)) / tan(z)
+    {
+        // Calculate value and partial derivatives using dual numbers
+        Duals<3, float> w = sin(x * cos(constant2 * y)) / tan(z);
+
+        // Calculate numeric partial derivatives using central differences
+        auto f = [](float x, float y, float z) 
+        {
+            return sin(x * cos(2.0f * y)) / tan(z);
+        };
+        float derivNumericX = (f(inputx + EPSILON, inputy, inputz) - f(inputx - EPSILON, inputy, inputz)) / (2.0f * EPSILON);
+        float derivNumericY = (f(inputx, inputy + EPSILON, inputz) - f(inputx, inputy - EPSILON, inputz)) / (2.0f * EPSILON);
+        float derivNumericZ = (f(inputx, inputy, inputz + EPSILON) - f(inputx, inputy, inputz - EPSILON)) / (2.0f * EPSILON);
+
+        // Calculate actual partial derivatives
+        float derivActualX = cos(inputx * cos(2.0f * inputy)) * cos(2.0f * inputy) / tan(inputz);
+        float derivActualY = cos(inputx * cos(2.0f * inputy)) * -2.0f * inputx * sin(2.0f * inputy) / tan(inputz);
+        float derivActualZ = sin(inputx * cos(2.0f * inputy)) / -(sin(inputz) * sin(inputz));
+
+        // Show value and derivatives
+        cout << "w = sin(x * cos(2 * y)) / tan(z) (x = " << inputx << ", y = " << inputy << ", z = " << inputz << ")\n";
+        cout << "  w = " << w.getValue() << '\n';
+        cout << "  dual# dw/dx = " << w.getDerivative(0) << '\n';
+        cout << "  dual# dw/dy = " << w.getDerivative(1) << '\n';
+        cout << "  dual# dw/dz = " << w.getDerivative(2) << '\n';
+        cout << "  actual dw/dx = " << derivActualX << '\n';
+        cout << "  actual dw/dy = " << derivActualY << '\n';
+        cout << "  actual dw/dz = " << derivActualZ << '\n';
+        cout << "  numeric dw/dx = " << derivNumericX << '\n';
+        cout << "  numeric dw/dy = " << derivNumericY << '\n';
+        cout << "  numeric dw/dz = " << derivNumericZ << '\n';
+    }
+}
+
+void TestTrig(float input)
+{
+    // Create a dual number as the value of x
+    Duals<1, float> x(input, 1); // Setting derivative of x to 1
+
+    // sin
+    {
+        // Calculate value and derivative using dual numbers
+        Duals<1, float> y = sin(x);
+
+        // Calculate numeric derivative using central differences
+        float derivNumeric = (sin(input + EPSILON) - sin(input - EPSILON)) / (2.0f * EPSILON);
+
+        // Calculate actual derivative
+        float derivActual = cos(input);
+
+        // Show value and derivatives
+        cout << "sin(" << input << ") = " << y.getValue() << '\n';
+        cout << "  dual# dy/dx = " << y.getDerivative() << '\n';
+        cout << "  actual dy/dx = " << derivActual << '\n';
+        cout << "  numeric dy/dx = " << derivNumeric << '\n';
+    }
+
+    // cos
+    {
+        // Calculate value and derivative using dual numbers
+        Duals<1, float> y = cos(x);
+
+        // Calculate numeric derivative using central differences
+        float derivNumeric = (cos(input + EPSILON) - cos(input - EPSILON)) / (2.0f * EPSILON);
+
+        // Calculate actual derivative
+        float derivActual = -sin(input);
+
+        // Show value and derivatives
+        cout << "cos(" << input << ") = " << y.getValue() << '\n';
+        cout << "  dual# dy/dx = " << y.getDerivative() << '\n';
+        cout << "  actual dy/dx = " << derivActual << '\n';
+        cout << "  numeric dy/dx = " << derivNumeric << '\n';
+    }
+
+    // tan
+    {
+        // Calculate value and derivative using dual numbers
+        Duals<1, float> y = tan(x);
+
+        // Calculate numeric derivative using central differences
+        float derivNumeric = (tan(input + EPSILON) - tan(input - EPSILON)) / (2.0f * EPSILON);
+
+        // Calculate actual derivative
+        float derivActual = 1.0f / (cos(input) * cos(input));
+
+        // Show value and derivatives
+        cout << "tan(" << input << ") = " << y.getValue() << '\n';
+        cout << "  dual# dy/dx = " << y.getDerivative() << '\n';
+        cout << "  actual dy/dx = " << derivActual << '\n';
+        cout << "  numeric dy/dx = " << derivNumeric << '\n';
+    }
+
+    // atan
+    {
+        // Calculate value and derivative using dual numbers
+        Duals<1, float> y = arctan(x);
+
+        // Calculate numeric derivative using central differences
+        float derivNumeric = (atan(input + EPSILON) - atan(input - EPSILON)) / (2.0f * EPSILON);
+
+        // Calculate actual derivative
+        float derivActual = 1.0f / (1.0f + input * input);
+
+        // Show value and derivatives
+        cout << "atan(" << input << ") = " << y.getValue() << '\n';
+        cout << "  dual# dy/dx = " << y.getDerivative() << '\n';
+        cout << "  actual dy/dx = " << derivActual << '\n';
+        cout << "  numeric dy/dx = " << derivNumeric << '\n';
+    }
+}
+
 int main() 
 {
-    TestSmoothStep<1, float>(0.5f);
-    TestSmoothStep<1, float>(0.75f);
+    TestSimpleFunction<1, float>(1.5f);
+    TestSimpleFunction<1, float>(2.75f);
 
-    float x_val = 2.0f;
-    float y_val = 3.0f;
-    TestTwoD(x_val, y_val);
+    TestTrig(PI / 4.0f);
 
-    Test2D(5.0f, 4.0f);
+    TestMath(4.0f);
+
+    TestTwoD(2.5f, 3.7f);
+
+    Test2D(5.5f, 6.28f);
+
+    Test3D(12.7f, 7.53f, 5.01f);
 
     return 0;
 }
